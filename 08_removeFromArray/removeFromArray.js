c3f35a04c0c4c8ccf425f86a2a6a0950d4f702fa
krloves <karen.lovedorial@gmail.com>
2025-11-15 21:34:18 +0800

    finish removeFromArray.js.js & removeFromArray.spec.js exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let arrayLen = array.length;
+    let toRemoveLen = toRemove.length;
+    let returnArray = [];
+
+    for (let i=0; i < arrayLen; i++) {
+        let shouldRemove = false;
+
+        for (let r=0; r < toRemoveLen; r++) {
+            if (array.at(i) === toRemove.at(r)) {
+                shouldRemove = true;
+                break;
+            }
+        }
+
+        if (shouldRemove == false) {
+            returnArray.push(array[i])
+        }
+    }
+
+    return returnArray
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
